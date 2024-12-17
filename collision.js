a1d06027a99efb2f4962e3d64ba25a37e7d8d7d5
LiveSquid <liamashok@gmail.com>
2024-12-16 20:09:21 -0500

    Collision animations

--- collision.js
+++ collision.js
@@ -7,28 +7,60 @@ canvas4.height = 700;
 const explosions = [];
 let canvasPosition = canvas4.getBoundingClientRect();
 
-class Explosions {
+class Explosion {
     constructor(x, y) {
-        this.x = x;
-        this.y = y;
         this.spriteWidth = 200;
         this.spriteHeight = 179;
         this.width = this.spriteWidth * 0.5;
         this.height = this.spriteHeight * 0.5;
+        this.x = x;
+        this.y = y;
         this.image = new Image();
         this.image.src = 'boom.png';
         this.frame = 0;
+        this.timer = 0;
+        this.angle = Math.random() * 6.2;
+        this.sound = new Audio();
+        this.sound.src = 'boom.wav';
+        
     }
     update() {
-        this.frame++;
-
+        if (this.frame === 0) this.sound.play();
+        this.timer++;
+        if (this.timer % 10 === 0) {
+            this.frame++;
+        }
     }
     draw() {
-        ctx4.draw(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+        ctx4.save();
+        ctx4.translate(this.x, this.y);
+        ctx4.rotate(this.angle);
+        ctx4.drawImage(this.image, this.spriteWidth * this.frame, 0, this.spriteWidth, this.spriteHeight, 0 - this.width * 0.5, 0 - this.height * 0.5, this.width, this.height);
+        ctx4.restore();
     }
 }
 
 window.addEventListener('click', function(e){
-    ctx4.fillStyle = 'white';
-    ctx4.fillRect(e.x - canvasPosition.left, e.y, 50, 50);
+   createAnimation(e);
 });
+
+function createAnimation(e) {
+    let positionX = e.x - canvasPosition.left;
+    let positionY = e.y - canvasPosition.top;
+    explosions.push(new Explosion(positionX, positionY));
+
+}
+
+function animate4() {
+    ctx4.clearRect(0, 0, canvas4.width, canvas4.height);
+    for (let i = 0; i < explosions.length; i++) {
+        explosions[i].update();
+        explosions[i].draw();
+        if (explosions[i].frame > 5) {
+            explosions.splice(i, 1);
+            i--;
+        }
+    }
+    requestAnimationFrame(animate4);
+};
+animate4();
